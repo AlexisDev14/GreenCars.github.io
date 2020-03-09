6ef59bbbe333e77800ddb2e005c30ffd3177b2d2
AlexisDev14 <alexis.dev14@gmail.com>
2020-03-09 11:05:30 +0100

    firest commit

---
+++ precache-manifest.35d697694b51bb6d789f5070ebeef186.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465d1ec4734eae5d8e19",
+    "url": "/css/app.486f4ed5.css"
+  },
+  {
+    "revision": "e1b57f108a46fefb640a",
+    "url": "/css/chunk-vendors.080bb6bd.css"
+  },
+  {
+    "revision": "95885884383806c8120577d28a592457",
+    "url": "/img/greencars_logo.95885884.png"
+  },
+  {
+    "revision": "e4f6d80599f60ea1ec5f3cae5195063c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465d1ec4734eae5d8e19",
+    "url": "/js/app.dfbe6312.js"
+  },
+  {
+    "revision": "e1b57f108a46fefb640a",
+    "url": "/js/chunk-vendors.6ae56a62.js"
+  },
+  {
+    "revision": "4402599f79d88bb4e3d9a07bf7dd1614",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
